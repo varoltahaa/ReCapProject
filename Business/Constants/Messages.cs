f267472548bd7fd09a18026be004f62775b2a14d
Taha Varol <tahavarol98@hotmail.com>
2021-02-24 12:42:14 +0300

    24 Şubat 2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,28 +10,34 @@ namespace Business.Constants
         public static string CarDailyPriceInvalid = "Lütfen Sıfırdan Büyük Bir Miktar Giriniz.";
         public static string CarUpdated = " Araç Başarıyla Güncellendi ";
         public static string CarDeleted = " Araç Başarıyla Silindi ";
-        public static string MaintenanceTim = "Sistem Bakımda";
         public static string CarListed = "Araçlar Başarıyla Listelendi";
-        public static string BrandListed = "Markalar Başarıyla Listelendi";
-        public static string ColorListed = "Renkler Başarıyla Listelendi";
+
+        public static string MaintenanceTim = "Sistem Bakımda";
+
         public static string RentalAdded = "Kiralama İşlemi Başarılı";
         public static string RentalDeleted = "Kiralama İşlemi Silme Başarılı";
         public static string RentalUpdated = "Kiralama İşlemi Güncelleme Başarılı";
         public static string RentalListed = "Kiralanan Araçlar Başarıyla Güncellendi";
+
         public static string UserAdded = "Kullanıcı Başarıyla Eklendi";
         public static string UserDeleted = "Kullanıcı Başarıyla Silindi";
         public static string UserUpdated = "Kullanıcı Başarıyla Güncellendi";
         public static string UserListed = "Kullanıcılar Başarıyla Listelendi";
-        public static string UserPasswordInvalid = "Şifre 8 Karakterden Az Olamaz!";
+        public static string UserPasswordInvalid = "Şifre en az 8 en fazla 24 karakter olmak zorunda en az 1 tane özel karakter(.,?+%-*/=) içermek zorunda ve büyük küçük harflerden oluşmak zorunda!";
+
         public static string CustomerAdded = "Müşteri Başarıyla Eklendi";
         public static string CustomerDeleted = "Müşteri Başarıyla Silindi";
         public static string CustomerUpdated = "Müşteri Başarıyla Güncellendi";
         public static string CustomerListed = "Müşteriler Başarıyla Listelendi";
+
         public static string BrandAdded = "Araç Markası Başarıyla Eklendi";
         public static string BrandDeleted = "Araç Markası Başarıyla Silindi";
         public static string BrandUpdated = "Araç Markası Başarıyla Güncellendi";
+        public static string BrandListed = "Markalar Başarıyla Listelendi";
+
         public static string ColorAdded = "Renk Başarıyla Eklendi";
         public static string ColorDeleted = "Renk Başarıyla Silindi";
         public static string ColorUpdated = "Renk Başarıyla Güncellendi";
+        public static string ColorListed = "Renkler Başarıyla Listelendi";
     }
 }
--- Business/ValidationRules/FluentValidation/UserValidator.cs
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -11,11 +11,15 @@ namespace Business.ValidationRules.FluentValidation
         public UserValidator()
         {
             RuleFor(c => c.EMail).EmailAddress();
-            RuleFor(c => c.Password).MinimumLength(8);
+            RuleFor(c => c.EMail).NotNull();
             RuleFor(c => c.FirstName).NotNull();
             RuleFor(c => c.LastName).NotNull();
             RuleFor(c => c.Password).NotNull();
-            RuleFor(c => c.EMail).NotNull();
+            RuleFor(c => c.Password).Length(8,24).WithMessage("Şifre en az 8 en fazla 24 karakter olmak zorunda");
+            RuleFor(c => c.Password).Matches("[A-Z]").WithMessage("Şifre büyük harfler içermek zorunda")
+                .Matches("[a-z]").WithMessage("Şifre küçük harfler içermek zorunda")
+                .Matches("[0-9]").WithMessage("Şifre rakam içermek zorunda")
+                .Matches("(?=.*?[.,=|;:<>+#?!@$%^&*-])").WithMessage("Şifre en az 1 tane özel karakter içermek zorunda" );
 
         }
     }
